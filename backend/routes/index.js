43a25153b32769683b1a6ef82c8f8352a16dc0ad
cmizell186 <chrislmiz311@gmail.com>
2022-04-03 22:36:22 -0700

    fixed random function

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -38,12 +38,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // ADDING XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-    router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
-    });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     router.get('/api/csrf/restore', (req, res) => {
+//         res.cookie('XSRF-TOKEN', req.csrfToken());
+//         return res.json({});
+//     });
+// }
 
 
 module.exports = router;
