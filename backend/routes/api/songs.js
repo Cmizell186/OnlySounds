8edadffa43be856e574bba16890521760c16f8e8
cmizell186 <chrislmiz311@gmail.com>
2022-04-06 09:42:10 -0700

    bug fixing

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -13,7 +13,7 @@ const validateSong = [
     check('title')
         .exists({ checkFalsy: true })
         .notEmpty()
-        .isLength({ max: 150, min:5 })
+        .isLength({ max: 150, min: 5 })
         .withMessage('Please provide a valid title.'),
     check('songUrl')
         .exists({ checkFalsy: true })
@@ -21,14 +21,14 @@ const validateSong = [
         .isURL()
         .withMessage('Please provide a valid songUrl.'),
     check('imageUrl')
-        .exists({checkFalsy: true})
+        .exists({ checkFalsy: true })
         .notEmpty()
         .isURL()
         .withMessage('Please provide a valid imageUrl.'),
 ];
 
 // CREATE functionallity
-router.post('/', validateSong, asyncHandler(async (req, res) => {
+router.post('/', validateSong, requireAuth, asyncHandler(async (req, res) => {
     const { userId, title, imageUrl, songUrl } = req.body;
     const song = Song.build({ userId, title, imageUrl, songUrl });
     let validationErrors = validationResult(req);
@@ -36,14 +36,14 @@ router.post('/', validateSong, asyncHandler(async (req, res) => {
     if (validationErrors.isEmpty()) {
         await song.save();
         return res.json('success');
-    } else{
+    } else {
         const errors = validationErrors.array().map(error => error.msg);
         return res.json(errors);
     }
 }))
 
 // READ functionallity
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/', requireAuth, asyncHandler(async (req, res) => {
     // get all songs from db
     const songsList = await Song.findAll({
         include: 'User'
@@ -52,14 +52,14 @@ router.get('/', asyncHandler(async (req, res) => {
 }))
 
 
-router.get('/:id', asyncHandler(async (req, res) => { //specific song page
+router.get('/:id', requireAuth, asyncHandler(async (req, res) => { //specific song page
     const id = req.params.id;
     const song = await Song.findByPk(id)
     return res.json(song);
 }))
 
 // UPDATE functionallity
-router.put(`/:id`, asyncHandler(async (req, res) => {
+router.put(`/:id`, requireAuth, asyncHandler(async (req, res) => {
     const id = req.params.id;
     const song = await Song.update(req.body, {
         where: { id: id }
@@ -69,8 +69,12 @@ router.put(`/:id`, asyncHandler(async (req, res) => {
 
 // DELETE functionallity
 
-router.delete(`/:id`, asyncHandler(async (req, res) => { // delete specific song
+router.delete(`/:id`, requireAuth, asyncHandler(async (req, res) => { // delete specific song
+    const id = req.params.id;
+    const song = await Song.findByPk(id);
 
+    song.destroy()
+    return res.json('success');
 }))
 
 module.exports = router;
