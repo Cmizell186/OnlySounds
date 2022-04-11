e0e47a3e54468c55f936f82ac62a09ffe326fd2f
cmizell186 <chrislmiz311@gmail.com>
2022-04-11 02:12:31 -0700

    songs list working correctly

--- frontend/src/components/SongList/index.js
+++ frontend/src/components/SongList/index.js
@@ -1,9 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './SongList.css';
 import { NavLink } from 'react-router-dom';
 import { getAllSongs } from '../../store/songs';
 import { useDispatch, useSelector } from 'react-redux';
 import image from '../../images/default-album-art.png';
+
+// Swiper React Components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+import { EffectCoverflow, Pagination } from "swiper";
+
+
 function SongList() {
     const dispatch = useDispatch();
     const songList = useSelector(state => state.song.songs)
@@ -15,20 +29,42 @@ function SongList() {
 
 
     return (
-        <div className='songList'>
-            {Object.values(songList)?.map((ele, idx) => (
-                <NavLink to={`/songs/${ele?.id}`} className='individual-song' key={idx}>
-                    <p className='song-title'>{ele?.title}</p>
-                    <div className='songImage'>
-                    {ele?.imageUrl ?
-                        <img src={ele?.imageUrl} height='150vh' width="150vw" ></img>
-                        :
-                        <img src={image} height='150vh' width='150vw'></img>
-                    }
-                    </div>
-                </NavLink>
-            ))}
-        </div>
+        <>
+            <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+            >
+                {Object.values(songList)?.map((ele, idx) => (
+                    <SwiperSlide key={idx}>
+                        <NavLink to={`songs/${ele?.id}`}>
+                        {ele?.imageUrl ?
+                            <img src={ele?.imageUrl}  width='300px' height='300px'/>
+                            :
+                            <img src={image} width='300px' height='300px'/>
+                        }
+                        </NavLink>
+                        <div>
+                            <p>{ele?.title}</p>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <div className='instructions-h1'>
+                <h1>🎧Select A Song🎧</h1>
+            </div>
+        </>
     )
 }
 
